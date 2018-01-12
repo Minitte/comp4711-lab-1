5e0ab4a81facd712ad66308819216c3c0259b634
Minitte <arcentalminitte@gmail.com>
2018-01-11 16:20:48 -0800

    added report formating

--- Student.php
+++ Student.php
@@ -39,12 +39,25 @@ class Student {
     
     /**
      * Calculates the average grade of this student 
-     * @return type the average grade
+     * @return float
      */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
             $total += $value;
         return $total / count($this->grades);
+    }
+    
+    /**
+     * Formats the student's information 
+     * @return string
+     */
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
     }
 }
