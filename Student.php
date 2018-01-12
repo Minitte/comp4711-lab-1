287307e12a8c50ec23dfd3b6c386f4b03386d1e6
Minitte <arcentalminitte@gmail.com>
2018-01-11 16:18:03 -0800

    added two functions
    - added add_email
    - added add_grade

--- Student.php
+++ Student.php
@@ -20,5 +20,20 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * Adjust or adds the email to the student's list of emails
+     * @param type $which index for the email
+     * @param type $address email address to be inserted
+     */
+    function add_email($which, $address) {
+        $this->emails[$which] = $address;
+    }
     
+    /**
+     * Adds a grade to the list of grades for this student
+     * @param type $grade the grade to be added
+     */
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
 }
